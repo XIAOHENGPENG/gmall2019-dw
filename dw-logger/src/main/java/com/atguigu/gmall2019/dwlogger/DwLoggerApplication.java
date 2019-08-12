360615370f9fda83764fabec4ed0fddcaa7cf63d
XIAOHENGPENG <1132007045@qq.com>
2019-08-12 21:07:51 +0800

    Initial commit

---
+++ dw-logger/src/main/java/com/atguigu/gmall2019/dwlogger/DwLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall2019.dwlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwLoggerApplication.class, args);
+    }
+
+}
